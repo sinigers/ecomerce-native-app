55ce4eaa8c36db65e818b53d9f06664fb3e8dbfb
Alexander Sinigerov <sinigers@yahoo.com>
2022-02-19 20:40:36 +0200

    material icons on ViewImageScreen

--- EcomerceNativeApp/app/screens/ViewImageScreen.js
+++ EcomerceNativeApp/app/screens/ViewImageScreen.js
@@ -1,9 +1,21 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, View, Image } from "react-native";
 import React from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export default function ViewImageScreen() {
   return (
     <View style={styles.container}>
+      <View style={styles.closeIcon}>
+        <MaterialCommunityIcons name="close" color="white" size={32} />
+      </View>
+      <View style={styles.deleteIcon}>
+        <MaterialCommunityIcons
+          name="trash-can-outline"
+          color="white"
+          size={32}
+        />
+      </View>
       <Image
         resizeMode="contain"
         style={styles.image}
@@ -14,9 +26,19 @@ export default function ViewImageScreen() {
 }
 
 const styles = StyleSheet.create({
+  closeIcon: {
+    position: "absolute",
+    top: 40,
+    left: 30,
+  },
   container: {
-    backgroundColor: "#131313",
+    backgroundColor: Colors.black,
     flex: 1,
+  },
+  deleteIcon: {
+    position: "absolute",
+    top: 40,
+    left: 30,
   },
   image: {
     width: "100%",
