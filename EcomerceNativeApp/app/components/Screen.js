52befb6899534a01903a48b16230c6bbdd4f7e2e
Alexander Sinigerov <sinigers@yahoo.com>
2022-02-19 21:20:43 +0200

    messageScren and screen = (safeAreaView for OS and android)component

---
+++ EcomerceNativeApp/app/components/Screen.js
@@ -0,0 +1,15 @@
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Constants from "expo-constants";
+
+export default function Screen({ children }) {
+  return <SafeAreaView style={styles.screen}>{children}</SafeAreaView>;
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    // paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    //    or
+    paddingTop: Constants.statusBarHeight,
+  },
+});
