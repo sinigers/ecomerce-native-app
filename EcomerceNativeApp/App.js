69eaf0fd12e2ea228118108b4d648cd61dc4b382
Alexander Sinigerov <sinigers@yahoo.com>
2022-02-23 15:15:04 +0200

    login Screen

--- EcomerceNativeApp/App.js
+++ EcomerceNativeApp/App.js
@@ -15,40 +15,12 @@ import AccountScreen from "./app/screens/AccountScreen";
 import AppTextInput from "./app/components/AppTextInput";
 import { useState } from "react";
 import AppPicker from "./app/components/AppPicker";
-
-const categories = [
-  {
-    label: "Furniture",
-    value: 1,
-  },
-  {
-    label: "Clothing",
-    value: 2,
-  },
-  {
-    label: "Cameras",
-    value: 3,
-  },
-];
+import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
-  const [category, setCategory] = useState(categories[0]);
-
   return (
     <Screen>
-      {/* <Card
-        title="red black mlka"
-        subTitle="300"
-        image={require("./app/assets/jacket.jpg")}
-      /> */}
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={(item) => setCategory(item)}
-        items={categories}
-        icon="apps"
-        placeholder="Category"
-      />
-      <AppTextInput icon="email" placeholder="email" />
+      <LoginScreen />
     </Screen>
   );
 }
---
+++ EcomerceNativeApp/app/screens/LoginScreen.js
@@ -0,0 +1,49 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import React, { useState } from "react";
+
+import Screen from "../components/Screen";
+import AppTextInput from "../components/AppTextInput";
+import AppButton from "../components/AppButton";
+
+export default function LoginScreen() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  return (
+    <Screen style={styles.container}>
+      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+      <AppTextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        icon="email"
+        keyboardType="email-address"
+        onChangeText={(text) => setEmail(text)}
+        placeholder="Email"
+        textContentType="emailAddress"
+      />
+      <AppTextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        icon="lock"
+        onChangeText={(text) => setPassword(text)}
+        placeholder="Password"
+        secureTextEntry={true}
+        textContentType="password"
+      />
+      <AppButton title="Login" onPress={() => console.log(email, password)} />
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 16,
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 20,
+  },
+});
